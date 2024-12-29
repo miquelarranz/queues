58abc4db1ed5f6c4501ffe28ac49a03409a9f047
Miquel Arranz <miquel.arranz@typeform.com>
2024-12-29 17:34:59 +0100

    chore: typo in the title

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Queue" },
+    { title: "Queues" },
     {
       name: "description",
       content: "Daily task management web app",
